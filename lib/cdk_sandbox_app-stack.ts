a8125c6555fa83d6bec0895358a2cc4fa770349b
paulvandyke <paulvandyke@paulvandykes-MacBook-Pro.local>
2026-07-01 15:06:56 -0400

    AWS CDK basic scaffolding

--- lib/cdk_sandbox_app-stack.ts
+++ lib/cdk_sandbox_app-stack.ts
@@ -1,10 +1,12 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib/core";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class CdkSandboxAppStack extends cdk.Stack {
+export class CdkSandboxApp extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    //TODO: Set CDK.Removal policy to destroy
 
     // The code that defines your stack goes here
 
